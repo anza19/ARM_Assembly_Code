55c9436c1344ef880885f576ee6cd93c6737ba1f
Anza <anza.k.9539@gmail.com>
2018-01-28 22:00:33 -0500

    Update addList.s

--- addList.s
+++ addList.s
@@ -1,19 +1,19 @@
-		.text
-		.global	_start
+			.text
+			.global	_start
 
 _start:
 			LDR R0, =START		//R0 has the starting address of memory
 			LDR R1,	[R0, #4]	//R1 has the size of R1
 			ADD R2,	R0, #8		//R2 is the pointer the first element in the list
-			MOV R3, #0			//R3 is our sum variable
+			MOV R3, #0		//R3 is our sum variable
 
 SUM:		
 			LDR R4, [R2]		//load list element into R4
 			ADD R3, R3, R4		//add element into sum
 			SUBS R1, R1, #1		//decrement pointer
-			BEQ END				//when 0, exit
+			BEQ END			//when 0, exit
 			ADD R2, R2, #4		//update pointer to next address
-			B SUM				//go back
+			B SUM			//go back
 
 END:		B END				//exit condition
 
